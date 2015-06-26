a885154291f2b23b2c67cf6799007c6ebfa3cc6e
SamKChang <samio54321@gmail.com>
2015-06-26 13:59:53 +0200

    initial upload

--- clearEconomist_ALL.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -r /home/samio/.config/google-chrome/Default/Storage/ext/gebjgjhbjedcomcajgpodjgfjgkepgpl/DE72CE5A8A4C/IndexedDB/https_app.economist.com_0.indexeddb.leveldb/*
--- test.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use Switch;
-use List::Util qw(min max);
-
-
-print "max",max (10, 100),"\n";
